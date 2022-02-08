2a0c5424b1442c2d7101f9230128d21cc88266e4
Thomas Nind <TZNind@dundee.ac.uk>
2022-02-08 12:32:30 +0000

    Added RDMP plugin project skeleton for IsIdentifiable

---
+++ IsIdentifiablePlugin/ExecuteCommandRunIsIdentifiable.cs
@@ -0,0 +1,23 @@
+﻿using IsIdentifiable.Options;
+using IsIdentifiable.Runners;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.Curation.Data;
+
+namespace IsIdentifiablePlugin;
+
+internal class ExecuteCommandRunIsIdentifiable : BasicCommandExecution
+{
+    public ExecuteCommandRunIsIdentifiable(IBasicActivateItems activator, ICatalogue catalogue) : base(activator)
+    {
+
+    }
+    public override void Execute()
+    {
+        var opts = new IsIdentifiableRelationalDatabaseOptions()
+        {
+            
+        };
+        var runner = new DatabaseRunner(opts);
+        runner.Run();
+    }
+}
