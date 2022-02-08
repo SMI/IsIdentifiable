15cc6e469c7d73b95ee2d9c05d017d7b1bf13187
Thomas Nind <TZNind@dundee.ac.uk>
2022-02-08 12:32:30 +0000

    Added RDMP plugin project skeleton for IsIdentifiable

---
+++ IsIdentifiablePlugin/IsIdentifiablePluginUI.cs
@@ -0,0 +1,21 @@
+﻿using Rdmp.Core;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.Curation.Data;
+
+namespace IsIdentifiablePlugin;
+
+class IsIdentifiablePluginUI : PluginUserInterface
+{
+    public IsIdentifiablePluginUI(IBasicActivateItems itemActivator) : base(itemActivator)
+    {
+    }
+
+    public override IEnumerable<IAtomicCommand> GetAdditionalRightClickMenuItems(object o)
+    {
+        if (o is Catalogue c)
+        {
+            yield return new ExecuteCommandRunIsIdentifiable(BasicActivator, c) { SuggestedCategory = "IsIdentifiable"};
+        }
+    }
+}
