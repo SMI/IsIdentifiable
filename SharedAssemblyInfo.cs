6d2d3fec262e6a861795a9584a321f8793842dfb
Thomas Nind <TZNind@dundee.ac.uk>
2022-02-10 14:20:45 +0000

    Updated to 0.0.2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")] // This one can have the extra build info after it
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")] // This one can have the extra build info after it
