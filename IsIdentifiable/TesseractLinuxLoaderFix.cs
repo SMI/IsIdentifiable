241aefe78bf4ebffe441a6ba1ca6b7e0fb66b9ba
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2022-08-20 22:51:21 +0000

    Implement unload, address tracking

--- IsIdentifiable/TesseractLinuxLoaderFix.cs
+++ IsIdentifiable/TesseractLinuxLoaderFix.cs
@@ -9,42 +9,73 @@ namespace IsIdentifiable;
 /// Work around legacy Tesseract Interop code relying on old libdl.so
 /// </summary>
 public class TesseractLinuxLoaderFix {
-    // TODO: Record load/unload in LibraryLoader.loadedAssemblies(string->intptr)
-    public static void Patch()
-    {
-	if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-		return; // Only apply patch on Linux
-        var harmony = new Harmony("uk.ac.dundee.hic.tesseract");
-        var ll = typeof(LibraryLoader);
-        var self = typeof(TesseractLinuxLoaderFix);
-        harmony.Patch(ll.GetMethod("LoadLibrary"), prefix: new HarmonyMethod(self.GetMethod(nameof(LoadLibraryPatch))));
-        harmony.Patch(ll.GetMethod("GetProcAddress"), prefix: new HarmonyMethod(self.GetMethod(nameof(GetProcAddressPatch))));
-        harmony.Patch(ll.GetMethod("FreeLibrary"), prefix: new HarmonyMethod(self.GetMethod(nameof(FreeLibraryPatch))));
-    }
+  private static Dictionary<string, IntPtr> loadedAssemblies;
 
-    public static bool LoadLibraryPatch(string fileName, string platformName, ref IntPtr __result)
-    {
-        __result = UnixLoadLibrary($"{AppDomain.CurrentDomain.BaseDirectory}/x64/lib{fileName}.so",2);
-	Console.Error.WriteLine($"Loading library '{AppDomain.CurrentDomain.BaseDirectory}/x64/lib{fileName}.so', result {__result}");
-        return false;
-    }
+  public static void Patch()
+  {
+    if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+		  return; // Only apply patch on Linux
+      var harmony = new Harmony("uk.ac.dundee.hic.tesseract");
+      var ll = typeof(LibraryLoader);
+      var self = typeof(TesseractLinuxLoaderFix);
+      loadedAssemblies=ll.GetField("loadedAssemblies").GetValue(LibraryLoader.Instance) as Dictionary<string, IntPtr>;
+      harmony.Patch(ll.GetMethod("LoadLibrary"), prefix: new HarmonyMethod(self.GetMethod(nameof(LoadLibraryPatch))));
+      harmony.Patch(ll.GetMethod("GetProcAddress"), prefix: new HarmonyMethod(self.GetMethod(nameof(GetProcAddressPatch))));
+      harmony.Patch(ll.GetMethod("FreeLibrary"), prefix: new HarmonyMethod(self.GetMethod(nameof(FreeLibraryPatch))));
+  }
 
-    public static bool GetProcAddressPatch(IntPtr dllHandle, string name, ref IntPtr __result)
-    {
-        __result = UnixGetProcAddress(dllHandle, name);
-	if (__result==IntPtr.Zero)
-		Console.Error.WriteLine($"WARN:Failed looking for function '{name}' in module at address {dllHandle}");
-        return false;
+  public static bool LoadLibraryPatch(string fileName, string platformName, ref IntPtr __result)
+  {
+    try {
+      __result = UnixLoadLibrary($"{AppDomain.CurrentDomain.BaseDirectory}/x64/lib{fileName}.so",2);
+    } catch (EntryPointNotFoundException e) {
+      __result = DLLoadLibrary($"{AppDomain.CurrentDomain.BaseDirectory}/x64/lib{fileName}.so",2);
     }
+    if (__result!=IntPtr.Zero)
+      loadedAssemblies.Add(fileName,__result);
+    Console.Error.WriteLine($"Loading library '{AppDomain.CurrentDomain.BaseDirectory}/x64/lib{fileName}.so', result {__result}");
+    return false;
+  }
 
-    public static bool FreeLibraryPatch(string fileName,ref bool __result)
-    {
-        __result = true; // TODO: UnixFreeLibrary(handle) != 0;
-        Console.Error.WriteLine($"TODO: Ignoring attempt to unload '{fileName}'");
-	return false;
+  public static bool GetProcAddressPatch(IntPtr dllHandle, string name, ref IntPtr __result)
+  {
+    try {
+      __result = UnixGetProcAddress(dllHandle, name);
+    } catch (EntryPointNotFoundException e) {
+      __result = DLGetProcAddress(dllHandle, name);
     }
+    if (__result==IntPtr.Zero)
+      Console.Error.WriteLine($"WARN:Failed looking for function '{name}' in module at address {dllHandle}");
+    return false;
+  }
 
-    
+  public static bool FreeLibraryPatch(string fileName,ref bool __result)
+  {
+    if (loadedAssemblies.Remove(fileName,out var handle))
+      try {
+        __result=UnixFreeLibrary(handle);
+      } catch (EntryPointNotFoundException e) {
+        __result=DLFreeLibrary(handle);
+      }
+    else {
+      __result=false;
+      Console.Error.WriteLine($"WARNING:Ignoring attempt to unload unknown library {fileName}");
+    }
+    return false;
+  }
+
+
+  [DllImport("dl", EntryPoint = "dlopen")]
+  private static extern IntPtr DLLoadLibrary(string fileName, int flags);
+
+  [DllImport("dl", EntryPoint = "dlclose", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+  private static extern int DLFreeLibrary(IntPtr handle);
+
+  [DllImport("dl", EntryPoint = "dlsym", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+  private static extern IntPtr DLGetProcAddress(IntPtr handle, string symbol);
+
+  [DllImport("dl", EntryPoint = "dlerror", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+  private static extern IntPtr DLGetLastError();
 
   [DllImport("c", EntryPoint = "dlopen")]
   private static extern IntPtr UnixLoadLibrary(string fileName, int flags);
