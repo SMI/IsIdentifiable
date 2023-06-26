61a6fb3b6e8245e82f8c1241ed1eb707feb5275e
Ruairidh MacLeod <ruairidh@rkm.dev>
2023-06-26 13:11:06 +0100

    merge DatabaseFailureFactory into calling class and refactor

--- IsIdentifiable/Runners/DatabaseRunner.cs
+++ IsIdentifiable/Runners/DatabaseRunner.cs
@@ -1,4 +1,4 @@
-﻿using FAnsi.Discovery;
+using FAnsi.Discovery;
 using FAnsi.Discovery.QuerySyntax;
 using IsIdentifiable.Options;
 using IsIdentifiable.Reporting;
@@ -18,12 +18,13 @@ namespace IsIdentifiable.Runners;
 public class DatabaseRunner : IsIdentifiableAbstractRunner
 {
     private readonly IsIdentifiableRelationalDatabaseOptions _opts;
-    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
+    private string _tableName;
     private DiscoveredColumn[] _columns;
     private string[] _columnsNames;
     private bool[] _stringColumns;
-    private DatabaseFailureFactory _factory;
+    private DiscoveredColumn[] _primaryKeys;
 
     /// <summary>
     /// Creates a new instance that will read from the 
@@ -46,14 +47,15 @@ public class DatabaseRunner : IsIdentifiableAbstractRunner
     /// <returns>0 if all went well</returns>
     public override int Run()
     {
-        var tbl = GetServer(_opts.DatabaseConnectionString, _opts.DatabaseType, _opts.TableName);
-        var server = tbl.Database.Server;
+        var table = GetServer(_opts.DatabaseConnectionString, _opts.DatabaseType, _opts.TableName);
+        _tableName = table.GetFullyQualifiedName();
 
-        _factory = new DatabaseFailureFactory(tbl);
+        var server = table.Database.Server;
 
-        _columns = tbl.DiscoverColumns();
+        _columns = table.DiscoverColumns();
         _columnsNames = _columns.Select(c => c.GetRuntimeName()).ToArray();
         _stringColumns = _columns.Select(c => c.GetGuesser().Guess.CSharpType == typeof(string)).ToArray();
+        _primaryKeys = _columns.Where(c => c.IsPrimaryKey).ToArray();
 
         using var con = server.GetConnection();
         con.Open();
@@ -68,7 +70,7 @@ public class DatabaseRunner : IsIdentifiableAbstractRunner
 {(top is { Location: QueryComponent.SELECT } ? top.SQL : "")}
 {string.Join($",{Environment.NewLine}", _columns.Select(c => c.GetFullyQualifiedName()))}
 FROM 
-{tbl.GetFullyQualifiedName()}
+{_tableName}
 {(top is { Location: QueryComponent.Postfix } ? top.SQL : "")}"
             , con);
 
@@ -76,33 +78,46 @@ FROM
 
         using var reader = cmd.ExecuteReader();
 
-        foreach (var failure in reader.Cast<DbDataRecord>().SelectMany(GetFailuresIfAny)) AddToReports(failure);
+        foreach (var failure in reader.Cast<DbDataRecord>().SelectMany(GetFailures))
+            AddToReports(failure);
 
         CloseReports();
+
         return 0;
     }
 
-    private IEnumerable<Reporting.Failure> GetFailuresIfAny(DbDataRecord record)
+    private IEnumerable<Failure> GetFailures(DbDataRecord record)
     {
         //Get the primary key of the current row
-        var primaryKey = _factory.PrimaryKeys.Select(k => record[k.GetRuntimeName()].ToString()).ToArray();
+        var primaryKey = _primaryKeys.Select(k => record[k.GetRuntimeName()].ToString()).Single();
 
         //For each column in the table
         for (var i = 0; i < _columnsNames.Length; i++)
-            //If it is a string column
-            if (_stringColumns[i])
+        {
+            //If it is not a string column
+            if (!_stringColumns[i])
+                continue;
+
+            var asString = record[i] as string;
+
+            if (string.IsNullOrWhiteSpace(asString))
+                continue;
+
+            // Some strings contain null characters?!  Remove them all.
+            // XXX hopefully this won't break any special character encoding (eg. UTF)
+            var parts = asString.Split('\\').SelectMany(part => Validate(_columnsNames[i], part.Replace("\0", ""))).ToList();
+
+            if (!parts.Any())
+                continue;
+
+            yield return new Failure(parts)
             {
-                var asString = record[i] as string;
-
-                if (string.IsNullOrWhiteSpace(asString))
-                    continue;
-
-                // Some strings contain null characters?!  Remove them all.
-                // XXX hopefully this won't break any special character encoding (eg. UTF)
-                var parts = asString.Split('\\').SelectMany(part => Validate(_columnsNames[i], part.Replace("\0", ""))).ToList();
-                if (parts.Any())
-                    yield return _factory.Create(_columnsNames[i], asString, parts, primaryKey);
-            }
+                Resource = _tableName,
+                ResourcePrimaryKey = primaryKey,
+                ProblemField = _columnsNames[i],
+                ProblemValue = asString,
+            };
+        }
 
         DoneRows(1);
     }
