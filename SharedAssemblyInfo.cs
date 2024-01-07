9ce9bd2f055d780a83b471c8ab3159e0c2e69f64
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-01-07 17:21:07 +0000

    v0.2.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Universities of Dundee and Edinburgh")]
-[assembly: AssemblyCopyright("Copyright © SMI Project 2018-2022")]
+[assembly: AssemblyCopyright("Copyright © SMI Project 2018-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
 [assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")] // This one can have the extra build info after it
+[assembly: AssemblyInformationalVersion("0.2.0")] // This one can have the extra build info after it
