f9f27cbee8acf06aef59e385d4c01f81269a7f6c
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-06-22 17:40:14 +0100

    use strings in options classes instead of (I)FileInfo

--- IsIdentifiable/Options/IsIdentifiableFileOptions.cs
+++ IsIdentifiable/Options/IsIdentifiableFileOptions.cs
@@ -14,7 +14,7 @@ public class IsIdentifiableFileOptions : IsIdentifiableOptions
     /// </summary>
 
     [Option('f', HelpText = "Path to a file to be evaluated", Required = true)]
-    public IFileInfo File { get; set; }
+    public string FilePath { get; set; }
 
     /// <summary>
     /// Optional.  The culture of dates, numbers etc if different from system culture
@@ -26,8 +26,8 @@ public class IsIdentifiableFileOptions : IsIdentifiableOptions
     /// Returns the name of the <see cref="File"/> (for use in outputted report names)
     /// </summary>
     /// <returns></returns>
-    public override string GetTargetName(IFileSystem _)
+    public override string GetTargetName(IFileSystem fileSystem)
     {
-        return File.Name;
+        return fileSystem.FileInfo.New(FilePath).Name;
     }
 }
--- ii/IsIdentifiableFileGlobOptions.cs
+++ ii/IsIdentifiableFileGlobOptions.cs
@@ -1,6 +1,5 @@
 ﻿using CommandLine;
 using IsIdentifiable.Options;
-using System.IO.Abstractions;
 
 namespace ii;
 
@@ -13,7 +12,7 @@ internal class IsIdentifiableFileGlobOptions : IsIdentifiableFileOptions
 {
     [Option('f', HelpText = "Path to a file or directory to be evaluated", Required = true)]
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public new IFileInfo File { get; set; }
+    public new string FilePath { get; set; }
 #pragma warning restore CS8618
 
     [Option('g', HelpText = "Pattern to use for matching files when -f is a directory.  Supports specifying a glob e.g. /**/*.csv", Required = false, Default = "*.csv")]
